8b2b3e7c2de9a378b7394a5b25c1cebefc230ab6
Awachi <aa03188@heron128.eps.surrey.ac.uk>
2020-03-03 16:09:58 +0000

    First test model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_03_160905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "homes", force: :cascade do |t|
+    t.string "home"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
